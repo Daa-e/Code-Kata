ecf5bb6c8972b40f98558e295038631d762986ce
Daa-e <80297289+Daa-e@users.noreply.github.com>
2026-03-27 09:21:26 +0900

    Add function to return reversed digits of a number
    Implement solution to reverse digits of a number.

---
+++ 26_03_27.c
@@ -0,0 +1,21 @@
+// 문제 설명
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+// 제한 조건 n은 10,000,000,000이하인 자연수입니다.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(long long n)
+{
+    int* answer = (int*)malloc(sizeof(int) * 12);
+    int i = 0;
+    
+    while (n > 0)
+    {
+        answer[i] = (int)(n % 10);
+        n /= 10;
+        i++;
+    }
+    return answer;
+}
