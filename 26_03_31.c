74a2d126e3996c5033c3f8b767a5d348cdb1cbe3
Daa-e <80297289+Daa-e@users.noreply.github.com>
2026-03-31 09:28:18 +0900

    Add function to determine perfect square and return result
    Implement function to check if n is a perfect square and return (x + 1)^2 or -1.

---
+++ 26_03_31.c
@@ -0,0 +1,23 @@
+// 문제 설명
+// 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+// n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+long long solution(long long n) {
+    long long answer = 0;
+
+    long long x = (long long)sqrt(n);
+    
+    if (x * x == n) {
+        answer = (x + 1) * (x + 1);
+    } else {
+        answer = -1;
+    }
+    
+    return answer;
+}
