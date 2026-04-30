31e6a2ec78b1b7c8d97daee328c391905f018131
Daa-e <80297289+Daa-e@users.noreply.github.com>
2026-04-30 09:56:43 +0900

    Add solution function for alternating case in words
    Implement function to format string with alternating case.

---
+++ 26_04_30.cpp
@@ -0,0 +1,31 @@
+// 문제 설명
+// 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 
+// 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s) {
+    int index = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            index = 0;
+            continue;
+        }
+
+        if (index % 2 == 0) {
+            s[i] = toupper(s[i]);
+        } else {
+            s[i] = tolower(s[i]);
+        }
+        
+        index++;
+    }
+
+    return s;
+}
